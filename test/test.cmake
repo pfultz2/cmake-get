40ce8463a137feab75a0be25d81839d7709f4125
Paul <pfultz2@yahoo.com>
2017-03-26 04:44:33 -0500

    Test update

--- test/test.cmake
+++ test/test.cmake
@@ -1,7 +1,9 @@
 
+string(RANDOM _TEST_RAND)
 set(TEST ${CMAKE_ARGV3})
+set(TMP_DIR ${CMAKE_ARGV4}-${_TEST_RAND})
 include(${CMAKE_CURRENT_LIST_DIR}/../share/cmake/cmakeget/CMakeGet.cmake)
-cget_mktemp_dir(TMP_DIR)
+file(MAKE_DIRECTORY ${TMP_DIR})
 set(TEST_DIR ${CMAKE_CURRENT_LIST_DIR})
 include(${TEST})
 file(REMOVE_RECURSE ${TMP_DIR})
