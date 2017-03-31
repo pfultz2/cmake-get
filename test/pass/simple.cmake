0eae4144d9e545476bc0a4f638c057889d9dd761
Paul <pfultz2@yahoo.com>
2017-03-30 23:58:36 -0500

    Dont install multiple times

--- test/pass/simple.cmake
+++ test/pass/simple.cmake
@@ -2,3 +2,5 @@
 set(_cmake_get_configure_reqs On)
 cmake_get(${TEST_DIR}/basicapp PREFIX ${TMP_DIR})
 test_expect_install_count(2)
+cmake_get(simple,${TEST_DIR}/libsimple PREFIX ${TMP_DIR})
+test_expect_install_count(2)
