c33235b5d176dff3435433c3618bc93537a8fcba
Paul <pfultz2@yahoo.com>
2017-08-15 13:17:34 -0500

    Set config

---
+++ test/libsimpledebug/include/simple.h
@@ -0,0 +1,14 @@
+
+#ifndef GUARD_SIMPLE_H
+#define GUARD_SIMPLE_H
+
+#ifdef NDEBUG
+#error "Not debug"
+#endif
+
+inline void simple()
+{
+}
+
+
+#endif
---
+++ test/pass/simpledebug.cmake
@@ -0,0 +1,4 @@
+
+set(_cmake_get_configure_reqs On)
+cmake_get(simple,${TEST_DIR}/libsimpledebug PREFIX ${TMP_DIR} CMAKE_ARGS -DCMAKE_BUILD_TYPE=debug)
+test_expect_install_count(1)
